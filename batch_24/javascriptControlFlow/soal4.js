dd007287fb23e49725db053001eb1d5d80dd5dea
daffabadrant9390 <daffabadrant@gmail.com>
2025-07-06 12:33:16 +0700

    feat: update soal4.js

--- batch_24/javascriptControlFlow/soal4.js
+++ batch_24/javascriptControlFlow/soal4.js
@@ -1,5 +1,5 @@
 /**
- * Hitung penjualan selama 12 bulan
+ * Hitung penjualan selama 6 bulan
  * - Terdapat array pendapatan dan pengeluaran
  * - Tentukan pendapatan bersih dari toko setiap bulannya (check yang mengalami kerugian)
  *  - Setiap bulan ganjil, ada tambahan pendapatan dari parkir sebesar 10% dari total pendapatan
@@ -10,3 +10,6 @@
  * - Bulan yang mengalami kerugian (masukan dalam array kerugian)
  * - Bulan yang mengalami keuntungan (masukan dlam array keuntungan)
  */
+
+const arrayPenjualan = [2000000, 5000000, 10000000, 7500000, 4750000, 1025050];
+const arrayPengeluaran = [1800000, 2000750, 4550000, 4750000, 5000000, 5150000];
